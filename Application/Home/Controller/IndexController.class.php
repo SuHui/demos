ff231318fff940e4538339eb939575117dd7816e
Andy Ann <qdayy@126.com>
2015-01-09 11:27:03 +0800

    添加说明

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -3,8 +3,12 @@ namespace Home\Controller;
 use Think\Controller;
 class IndexController extends Controller {
     public function index($p = ''){
-        if (IS_AJAX) {
-            echo '这是第'.$p.'页';
+        if ($p) { //请求第$p页的详细信息
+            if (IS_AJAX) { //pjax发送过来的请求都是Ajax请求
+                echo '这是第'.$p.'页';
+            } else {
+                echo '<head><meta charset="utf-8"></head>看到这个是因为浏览器不支持pjax，进行了跳转';
+            }
             return;
         }
 
